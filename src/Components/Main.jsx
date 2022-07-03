2a45dfe9ce419e3462befb3395a090df4962153a
Barry Mitchell <barry@clarusway.com>
2022-07-03 04:46:13 +0300

    add nextui

---
+++ src/Components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Products } from './Products';
+import { ProductsUI } from './ProductsUI';
+
+const Main = () => {
+  return (
+    <div>
+      <h1 style={{ fontSize: '2rem' }}>Clarusway Shopping</h1>
+      {/* <Products /> */}
+      <ProductsUI />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Components/ProductsUI.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
+
+export const ProductsUI = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    console.log('useEffect');
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+        console.log(data.products);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(products);
+  return (
+    <Grid.Container gap={2} justify="flex-start">
+      {products.map((item, index) => (
+        <Grid xs={6} sm={3} key={index}>
+          <Card isPressable>
+            <Card.Body css={{ p: 0 }}>
+              <Card.Image
+                src={item.thumbnail}
+                objectFit="cover"
+                width="100%"
+                height={140}
+                alt={item.title}
+              />
+            </Card.Body>
+            <Card.Footer css={{ justifyItems: 'flex-start' }}>
+              <Row wrap="wrap" justify="space-between" align="center">
+                <Text b>{item.title}</Text>
+                <Text
+                  css={{
+                    color: '$accents7',
+                    fontWeight: '$semibold',
+                    fontSize: '$sm',
+                  }}
+                >
+                  {item.price}
+                </Text>
+              </Row>
+            </Card.Footer>
+          </Card>
+        </Grid>
+      ))}
+    </Grid.Container>
+  );
+};
